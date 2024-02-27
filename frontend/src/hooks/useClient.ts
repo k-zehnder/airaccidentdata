45e6ee67d37c4138d7292c54d4e91def4b440ecf
Kevin Zehnder <kjzehnder3@gmail.com>
2024-02-26 22:40:13 -0800

    fix url

--- frontend/src/hooks/useClient.ts
+++ frontend/src/hooks/useClient.ts
@@ -13,8 +13,10 @@ interface AccidentDetails {
 interface APIClient extends AxiosInstance {}
 
 export const useClient = (): APIClient => {
+  const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/v1': 'https://airaccidentdata.com/api/v1'
+  
   return axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: baseURL,
     headers: {
       'Content-Type': 'application/json',
     },
