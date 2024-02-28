7371ba12a5f2f1496b6318120857a924606d1ca1
Kevin Zehnder <kjzehnder3@gmail.com>
2024-02-27 23:00:51 -0800

    somewhat better dark mode pagination and also improved badges

--- frontend/src/components/pagination.tsx
+++ frontend/src/components/pagination.tsx
@@ -1,12 +1,14 @@
+// Pagination component
 import React from 'react';
 
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
   onPageChange: (page: number) => void;
+  darkMode?: boolean; 
 }
 
-const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange }) => {
+const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPageChange, darkMode }) => {
   // Function to create page numbers
   const getPageNumbers = () => {
     const pages = [];
@@ -21,7 +23,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
       {currentPage > 1 && (
         <button
           onClick={() => onPageChange(currentPage - 1)}
-          className="mx-2 px-4 py-2 bg-foreground text-white rounded hover:bg-foreground focus:outline-none focus:ring-2 focus:bg-foreground focus:ring-opacity-50"
+          className={`mx-2 px-4 py-2 ${darkMode ? 'bg-gray-800 text-white' : 'bg-foreground text-white'} rounded hover:bg-foreground focus:outline-none focus:ring-2 focus:bg-foreground focus:ring-opacity-50`}
         >
           Previous
         </button>
@@ -31,7 +33,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
         <button
           key={page}
           onClick={() => onPageChange(page)}
-          className={`mx-1 px-3 py-2 text-sm font-medium ${currentPage === page ? 'bg-foreground text-white' : 'bg-white text-gray-700'} rounded hover:bg-foreground hover:text-white focus:outline-none focus:ring-2 focus:ring-foreground focus:ring-opacity-50`}
+          className={`mx-1 px-3 py-2 text-sm font-medium ${currentPage === page ? darkMode ? 'bg-gray-800 text-white' : 'bg-foreground text-white' : 'bg-white text-gray-700'} rounded hover:bg-foreground hover:text-white focus:outline-none focus:ring-2 focus:ring-foreground focus:ring-opacity-50`}
         >
           {page}
         </button>
@@ -40,7 +42,7 @@ const Pagination: React.FC<PaginationProps> = ({ currentPage, totalPages, onPage
       {currentPage < totalPages && (
         <button
           onClick={() => onPageChange(currentPage + 1)}
-          className="mx-2 px-4 py-2 bg-foreground text-white rounded hover:bg-foreground focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+          className={`mx-2 px-4 py-2 ${darkMode ? 'bg-gray-800 text-white' : 'bg-foreground text-white'} rounded hover:bg-foreground focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50`}
         >
           Next
         </button>
