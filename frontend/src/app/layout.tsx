2b726af446890a30ee8ce16c4ad866b577747735
Kevin Zehnder <kjzehnder3@gmail.com>
2024-04-25 09:56:50 -0700

    `feat`: optimize SEO metadata for enhanced web presence (#74)

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,40 +1,72 @@
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { cn } from "../lib/utils";
-import Head from 'next/head'; 
+'use client';
 
-// Define the font styling using CSS-in-JS or a separate CSS class
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '../lib/utils';
+import Head from 'next/head';
+import { usePathname } from 'next/navigation';
+
 const fontStyling = {
   fontFamily: '"Inter", sans-serif',
 };
 
-export const metadata = {
-  title: "airaccidentdata.com",
-  description: "Latest plane crash accidents and incident news!",
-  faviconUrl: "http://s.airaccidentdata.com/og-image.png",
+const metadata = {
+  title: 'airaccidentdata.com',
+  description: 'Latest plane crash accidents and incident news!',
+  siteUrl: 'http://airaccidentdata.com',
+  ogImage:
+    'https://s3.us-west-1.amazonaws.com/s.airaccidentdata.com/og-image.png',
 };
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-export default async function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: RootLayoutProps) {
+  const pathname = usePathname();
 
   return (
-      <html lang="en">
-        <body
-          className={cn("min-h-screen bg-background antialiased")}
-          style={fontStyling}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <div className="flex w-full flex-1 flex-col overflow-hidden">
-                <link rel="icon" href="https://s.airaccidentdata.com/favicon.ico" type="image/xicon" sizes="16x16" />
-                {children}
-              </div>
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={metadata.description} />
+        <meta property="og:url" content={`${metadata.siteUrl}${pathname}`} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta
+          property="og:image"
+          content={`${metadata.siteUrl}${metadata.ogImage}`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta
+          name="twitter:image"
+          content={`${metadata.siteUrl}${metadata.ogImage}`}
+        />
+        <link rel="canonical" href={`${metadata.siteUrl}${pathname}`} />
+        <link
+          rel="icon"
+          href={`${metadata.siteUrl}/favicon.ico`}
+          type="image/x-icon"
+          sizes="16x16"
+        />
+      </Head>
+      <body
+        className={cn('min-h-screen bg-background antialiased')}
+        style={fontStyling}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="relative flex min-h-screen flex-col">
+            <div className="flex w-full flex-1 flex-col overflow-hidden">
+              {children}
             </div>
-          </ThemeProvider>
-        </body>
-      </html>
-   );
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
