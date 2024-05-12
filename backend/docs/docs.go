6faa4276fd1454e94faca1a173bcdb802a95bf17
Kevin Zehnder <kjzehnder3@gmail.com>
2024-05-12 13:30:02 -0700

    fix: Add Swagger annotations to main.go (#124)

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -539,12 +539,12 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
+	Version:          "1",
 	Host:             "",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
-	Title:            "Air Accident Data API",
-	Description:      "This is the server for managing air accident data.",
+	Title:            "AirAccidentData.com API",
+	Description:      "API server for managing air accident data.",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,16 @@
 // Package main runs the AirAccidentData API server.
+// @title AirAccidentData.com API
+// @description API server for managing air accident data.
+// @version 1
+// @BasePath /api/v1
 package main
 
 import (
 	"log"
 
-	_ "github.com/computers33333/airaccidentdata/docs" // Enables Swagger documentation generation.
 	"github.com/joho/godotenv"
 
+	"github.com/computers33333/airaccidentdata/docs"
 	"github.com/computers33333/airaccidentdata/internal/api/server"
 	"github.com/computers33333/airaccidentdata/internal/config"
 	"github.com/computers33333/airaccidentdata/internal/store"
@@ -14,18 +18,22 @@ import (
 
 // main sets up and starts the API server.
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
 	}
 
+	// Load configuration settings
 	cfg := config.NewConfig()
 
+	// Configure Swagger and initialize the database
+	docs.SwaggerInfo.Host = cfg.SwaggerHost
 	store, err := store.NewStore(cfg.DataSourceName)
 	if err != nil {
 		log.Fatalf("Failed to create store: %v", err)
 	}
 
+	// Initialize and start the HTTP server
 	router := server.NewServer(store)
 	srv := server.StartServer(":8080", router)
 	defer server.GracefulShutdown(srv)
